0035e086db91bcc95aeab86dc1eaf2fe938a294b
Vic <ludivico.oguan@softvision.com>
2022-12-02 14:51:20 +0800

    configure application properties

--- src/main/java/com/example/GitPractice/repository/ClassmateRepository.java
+++ src/main/java/com/example/GitPractice/repository/ClassmateRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClassmateRepository extends JpaRepository<Classmate, Long> {
+public interface  ClassmateRepository extends JpaRepository<Classmate, Long> {
 }
