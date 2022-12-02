3fd85e04da412dfbaf440153ac68f8a1a16d7a1d
Vic <ludivico.oguan@softvision.com>
2022-12-02 14:15:05 +0800

    Edited Model with Annotations

--- src/main/java/com/example/GitPractice/model/Classmate.java
+++ src/main/java/com/example/GitPractice/model/Classmate.java
@@ -1,12 +1,17 @@
 package com.example.GitPractice.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 public class Classmate {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column
     private Long id;
+    @Column
     private String name;
+    @Column
     private String section;
 }
